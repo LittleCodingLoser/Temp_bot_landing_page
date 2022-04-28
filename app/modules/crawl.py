a8bda03e53a308149a759f54caa5333595a15cb0
LittleCodingLoser <s0975247623@gmail.com>
2022-04-29 01:42:55 +0800

    env experiment again

--- app/modules/crawl.py
+++ app/modules/crawl.py
@@ -15,6 +15,7 @@ def check_health_leader(health_leader_class, account, password):
         # Chrome Driver 路徑
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        print(os.environ.get("GOOGLE_CHROME_BIN"))
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
